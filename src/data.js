70cdd2d3236f5ea70057e15bb72b6045e2604429
DEID4 <dariuspasieka@gmail.com>
2019-11-15 19:46:08 +0100

    initial commit

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export default
+    {
+        name :'Dariusz Pasieka',
+        headerTagline: [
+                        'Welcome to my website. I am Dariusz Pasieka',
+                        'Graduate Masters Student in Computing',              
+                        'From Poland'
+    ],
+        abouttext: 'I am a Cardiff Metropolitan University student who graduated studying Computing. My award is Distinction Master of Science in Computing. I have also finished Bachelor in Computing in Cardiff Metropolitan University with the upper second class. I am passionate about programming, technology and video games. Currently looking for an opportunity to work as a front-end developer or data analyst.',
+        aboutImage:'https://i.imgur.com/i2EggDy.png',
+       ShowAboutImage:true,
+       projects:[
+           {
+            id: 1,
+            title:'Project One', 
+             service:'Mapping Population Distribution', 
+             imageSrc:"https://i.imgur.com/FS7C4fr.png",
+             url:'https://github.com/DPasieka/Mapping-Population-Distribution'
+            },
+            {
+                id: 2,
+                title: 'Project Two',
+                service: 'Analysis of spheroid',
+                imageSrc: "https://i.imgur.com/3qf1Ryg.jpg",
+                url: 'https://github.com/DPasieka/Analysis-of-spheroid'
+            },
+            
+        ],
+        social: [
+            {   name:'Github',
+                url:'https://github.com/DPasieka/'},
+            {
+                name: 'Linkedin',
+                url: 'https://www.linkedin.com/in/dariusz-pasieka-b98322172/'
+            },
+
+        ]
+    }
