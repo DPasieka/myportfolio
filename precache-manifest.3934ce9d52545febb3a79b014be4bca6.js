9af723cb83e43984cda1533738b818fa079b8710
DEID4 <dariuspasieka@gmail.com>
2019-11-15 19:49:06 +0100

    Updates

---
+++ precache-manifest.3934ce9d52545febb3a79b014be4bca6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce3cfaeee0598a58b31ae95afcbdc72",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "30b4453ed31cfe63d6bb",
+    "url": "/myportfolio/static/css/main.a2465c54.chunk.css"
+  },
+  {
+    "revision": "fc461e4a67800bc54a84",
+    "url": "/myportfolio/static/js/2.0abf5e3f.chunk.js"
+  },
+  {
+    "revision": "30b4453ed31cfe63d6bb",
+    "url": "/myportfolio/static/js/main.f6b76bd9.chunk.js"
+  },
+  {
+    "revision": "314136164c1c0c643422",
+    "url": "/myportfolio/static/js/runtime~main.69c47bdd.js"
+  }
+]);
